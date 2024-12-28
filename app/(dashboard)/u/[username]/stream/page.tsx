eb1575f40cda2dbcc1db551ee6338998466b3e4b
RaphaelKaira <113975698+Raphael-pix@users.noreply.github.com>
2024-12-28 22:41:13 +0300

    show stream modal if no stream

--- app/(dashboard)/u/[username]/stream/page.tsx
+++ app/(dashboard)/u/[username]/stream/page.tsx
@@ -1,6 +1,7 @@
 import { getUserByUsername } from "@/lib/user-service";
 import StreamPlayer from "@/components/stream-player";
 import { currentUser } from "@clerk/nextjs/server";
+import StreamModal from "@/components/stream-modal";
 
 interface CreatorPageProps {
   params: Promise<{
@@ -14,7 +15,7 @@ const CreatorPage = async ({ params }: CreatorPageProps) => {
   const user = await getUserByUsername(username);
 
   if (!user || user.externalUserId !== externalUser?.id || !user.stream) {
-    throw new Error("Unauthorized");
+    return <StreamModal stream={null}/>
   }
 
   return (
